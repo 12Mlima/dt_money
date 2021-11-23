cb85507b42f6fa873b58ad6e2d0e5e66e4581706
12Mlima <marsellima@gmail.com>
2021-11-23 00:17:41 -0300

    feat: inicio da construção do modal de transações

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import { Dashboard } from './components/Dashboard/Index'
 import { Header } from './components/Header/Index'
 import { GlobalStyle } from './styles/global'
+import Modal from 'react-modal'
+import { useState } from 'react'
+import { NewTransactionModal } from './components/NewTransactionModal/Index'
+
+Modal.setAppElement('#root')
 
 export function App() {
+  const [isNewTransactionsOpen, setIsNewTransactionsOpen] = useState(false)
+
+  function handleOpenModal() {
+    setIsNewTransactionsOpen(true)
+  }
+
+  function handleCloseModal() {
+    setIsNewTransactionsOpen(false)
+  }
+
   return (
     <>
-      <Header />
+      <Header handleOpenModal={handleOpenModal} />
       <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionsOpen}
+        onRequestClose={handleCloseModal}
+      />
       <GlobalStyle />
     </>
   )
