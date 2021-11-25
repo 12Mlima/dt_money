1218be7007abd9aeac31e08a9333bf5b22581df6
12Mlima <marsellima@gmail.com>
2021-11-24 21:49:56 -0300

    feat: listando dados utilizando o mirageJS, e terminando a estilização do modal

--- src/index.tsx
+++ src/index.tsx
@@ -2,22 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
 
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de web',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+      ],
+    })
+  },
+
   routes() {
     this.namespace = 'api'
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transação 1',
-          amount: 400,
-          type: 'deposito',
-          category: 'comida',
-          createdAt: new Date(),
-        },
-      ]
+      return this.schema.all('transaction')
+    })
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+      return schema.create('transaction', data)
     })
   },
 })
